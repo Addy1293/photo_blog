06875b64e8a0ea97346cbe23a3178227de1770d3
Addy1293 <mrcool82@gmail.com>
2019-02-07 18:45:14 +0200

    functionality added

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,28 @@ exports.home = function (req, res){
 // 1st photography pictures 
 exports.each_picture = function(req, res) {
     var each_picture = req.params.each_picture;
-    res.send("Page for each picture " + each_picture);
+
+    var photos = photosJSON.photos;
+
+    if (each_picture >= 1 && each_picture <= 6) {
+        // This gets the picture description each time a diffent number is placed. 
+        var photo = photos[each_picture - 1];
+        // Specifying var title for the picture.
+        var title = photo.title;
+
+        var main_characters = photo.main_characters;
+
+        res.render('each_picture', {
+            photos : photos,
+            title : title,
+            photo : photo,
+            main_characters : main_characters
+        });
+        // The name on the left is the variable accesible & value on the right is what it represents.
+    } else {
+        res.send("This isnt the page you are look for. Try 1-6 please.");
+    }
+
 };
 
 // notFound
